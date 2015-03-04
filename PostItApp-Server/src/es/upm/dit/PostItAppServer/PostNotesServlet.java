781ad0f6792243409cb7dc9a35be514f7e310b65
rubolmu <ruben.oliva.munoz@alumnos.upm.es>
2015-03-04 17:10:47 +0100

    adding postnotesservlet

---
+++ PostItApp-Server/src/es/upm/dit/PostItAppServer/PostNotesServlet.java
@@ -0,0 +1,45 @@
+package es.upm.dit.PostItAppServer;
+
+import java.io.IOException;
+
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+
+// IMPORTAR LOS PAQUETES DAO.
+
+public class PostNotesServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L; //Serializes objects through HTTP.
+	
+	public void doPost (HttpServletRequest req, HttpServletResponse resp) throws IOException{ // Uploads the content of a Post-It
+		System.out.println("Creating new Post-It ");
+				
+		String title = checkNull(req.getParameter("title")); // Checks if the parameters are or not empty.
+		String text = checkNull (req.getParameter ("text"));
+		String lat = checkNull (req.getParameter("lat"));
+		String lon  = checkNull (req.getParameter("lon"));
+		
+		//AÑADIR LA GEOLOCALIZACION
+		
+		
+		
+		// INTRODUCIR LOS METODOS DEL DAO, Y EL REDIRECT...
+		
+		resp.sendRedirect("/"); // Loads the index page.
+		
+	}
+	private String checkNull (String s){ 
+		if (s == null){ 
+			return "This field should not be empty";
+		}
+		return s;
+	}
+	
+}
