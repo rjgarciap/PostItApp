0bc2d94e1c9405e371e0ebd5fac9486b16032f93
Moisés TG <moisestg93@gmail.com>
2015-03-04 10:33:13 +0100

    Añadiendo GetNoteServlet

---
+++ PostItApp-Server/src/es/upm/dit/PostItAppServer/GetNoteServlet.java
@@ -0,0 +1,28 @@
+package es.upm.dit.PostItAppServer;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+public class GetNoteServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		
+		// Recupero de la petición HTTP el id de la nota que quiere ver el usuario
+		String id = req.getParameter("id");
+		
+		// OJO a los nombres aquí
+		NoteDAO dao = NoteDAOImpl.getInstance();
+		
+		// OJO que este método no venía en lo de los TODOs
+		Note note = dao.getById(id);
+		
+		resp.setContentType("application/json");
+		
+		// OJO Abrá que añadir la geolocalización
+		resp.getWriter().println("{\"title\":" + note.title + ", \"text\":" + note.text + "}");
+		
+	}	
+}
